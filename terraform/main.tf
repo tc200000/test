6f0c3267d65e97c17052d95528957613837131a3
tc200000 <151476329+tc200000@users.noreply.github.com>
2024-12-08 17:59:13 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,21 @@
 terraform {
-
  required_providers {
    aws = {
      source  = "hashicorp/aws"
      version = "~> 3.0"
    }
  }
-
  required_version = ">= 1.2.0"
 }
-
 
 provider "aws" {
  region = "eu-central-1"
 }
 
-
 resource "aws_instance" "myec2" {
  ami = "ami-067e8d934c362b31e"
  instance_type = "t2.micro"
-
-# Додаємо Security Group ID
  vpc_security_group_ids = ["sg-09c59443e4e37de04"]
-
  tags = {
    name = "Demo System"
  }
