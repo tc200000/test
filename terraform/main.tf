b88ba4be7a9dc159022576fb8c3d458418394002
tc200000 <151476329+tc200000@users.noreply.github.com>
2024-12-22 21:28:29 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,40 +1,36 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-  required_version = ">= 1.2.0"
+ required_providers {
+   aws = {
+     source  = "hashicorp/aws"
+     version = "~> 3.0"
+   }
+ }
+ required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-  region = "eu-central-1"
-}
-
-# Використання змінної для AMI
-variable "ami_id" {
-  description = "The AMI ID for the EC2 instance"
-  type        = string
+ region = "eu-central-1"
 }
 
 resource "aws_instance" "myec2" {
-  ami                    = var.ami_id
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = ["sg-09c59443e4e37de04"] # Замініть на правильний ID групи безпеки
-  tags = {
-    Name = "Demo System"
-  }
-}
+ ami = "ami-04c522ee4750da4dc"
+ instance_type = "t2.micro"
+ vpc_security_group_ids = ["sg-09c59443e4e37de04"]
+ tags = {
+   name = "Demo System"
+ }
 
 # Виведення публічної IP-адреси
 output "public_ip" {
-  value       = aws_instance.myec2.public_ip
+  value = aws_instance.apache2_instance.public_ip
   description = "Public IP address of the created instance"
 }
 
 # Виведення приватної IP-адреси
 output "private_ip" {
-  value       = aws_instance.myec2.private_ip
+  value = aws_instance.apache2_instance.private_ip
   description = "Private IP address of the created instance"
+}
+
+
 }
